d814575670ea9eb5beb79a9ca2d9cf011acac4a9
= <isaabqari@gmail.com>
2020-06-03 10:17:39 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+download mysql data: https://sp.mysqltutorial.org/wp-content/uploads/2018/03/mysqlsampledatabase.zip
---
+++ src/test/java/com/fluex404/springbootdatatables/SpringbootDatatablesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluex404.springbootdatatables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDatatablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
